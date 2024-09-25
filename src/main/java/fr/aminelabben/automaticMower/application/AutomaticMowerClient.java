200af82dec6ff27bd3dc151247f04533f993c13b
Mohamed Amine LABBEN <mohamedaminelabben@MAC655.local>
2024-09-25 16:34:26 +0200

    Initial commit

---
+++ src/main/java/fr/aminelabben/automaticMower/application/AutomaticMowerClient.java
@@ -0,0 +1,9 @@
+package fr.aminelabben.automaticMower.application;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class AutomaticMowerClient {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to automatic mower!");
+    }
+}
