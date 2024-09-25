698b351956795fc6a919eb243e9140865c1d1f01
aminelabben <labben.ma@sfeir.com>
2024-09-25 21:46:31 +0200

    add object values Area and Position to the domain

---
+++ src/main/java/org/MowItNow/automaticMower/domain/Area.java
@@ -0,0 +1,4 @@
+package org.mowitnow.automaticmower.domain;
+
+public record Area(int width, int height) {
+}
---
+++ src/main/java/org/MowItNow/automaticMower/domain/Position.java
@@ -0,0 +1,4 @@
+package org.mowitnow.automaticmower.domain;
+
+public record Position(int x, int y) {
+}
