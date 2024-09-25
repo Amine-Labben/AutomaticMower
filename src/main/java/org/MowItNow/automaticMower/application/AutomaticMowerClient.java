429b74881282510b3dce6addbf048632098b8535
Mohamed Amine LABBEN <mohamedaminelabben@mac655.local>
2024-09-25 16:34:26 +0200

    Initial commit

---
+++ src/main/java/org/MowItNow/automaticMower/application/AutomaticMowerClient.java
@@ -0,0 +1,7 @@
+package org.mowitnow.automaticmower.application;
+
+public class AutomaticMowerClient {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to automatic mower!");
+    }
+}
