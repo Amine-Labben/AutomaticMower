f56dc0297ebf91551cdaafd184325e8460797842
aminelabben <labben.ma@sfeir.com>
2024-09-25 21:02:09 +0200

    Start Domain with orientation logic

---
+++ src/test/java/org/mowitnow/automaticmower/domain/OrientationTest.java
@@ -0,0 +1,49 @@
+package org.mowitnow.automaticmower.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class OrientationTest {
+
+    public static Stream<Arguments> provideRotationLeftArguments() {
+        return Stream.of(
+                Arguments.of(Orientation.NORTH, Orientation.WEST),
+                Arguments.of(Orientation.EST, Orientation.NORTH),
+                Arguments.of(Orientation.SOUTH, Orientation.EST),
+                Arguments.of(Orientation.WEST, Orientation.SOUTH)
+        );
+    }
+
+    public static Stream<Arguments> provideRotationRightArguments() {
+        return Stream.of(
+                Arguments.of(Orientation.NORTH, Orientation.EST),
+                Arguments.of(Orientation.EST, Orientation.SOUTH),
+                Arguments.of(Orientation.SOUTH, Orientation.WEST),
+                Arguments.of(Orientation.WEST, Orientation.NORTH)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRotationLeftArguments")
+    public void should_rotate_orientation_left(Orientation initialOrientation, Orientation expectedOrientation) {
+        // THEN
+        Orientation actualOrientation = initialOrientation.rotateLeft();
+
+        // WHEN
+        Assertions.assertEquals(expectedOrientation, actualOrientation);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRotationRightArguments")
+    public void should_rotate_orientation_right(Orientation initialOrientation, Orientation expectedOrientation) {
+        // THEN
+        Orientation actualOrientation = initialOrientation.rotateRight();
+
+        // WHEN
+        Assertions.assertEquals(expectedOrientation, actualOrientation);
+    }
+}
