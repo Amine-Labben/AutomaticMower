65510f46af532e3bc59eac21aa59a841b6f86c4e
aminelabben <labben.ma@sfeir.com>
2024-09-26 18:13:30 +0200

    Implement application layer to orchestrate process and display result

--- src/main/java/org/MowItNow/automaticMower/application/AutomaticMowerClient.java
+++ src/main/java/org/MowItNow/automaticMower/application/AutomaticMowerClient.java
@@ -1,7 +1,22 @@
 package org.mowitnow.automaticmower.application;
 
+import org.mowitnow.automaticmower.domain.MowingAreaService;
+import org.mowitnow.automaticmower.infrastructure.MowInputParser;
+
+import java.util.List;
+
 public class AutomaticMowerClient {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome to automatic mower!");
+    private static String DEFAULT_FILE_NAME = "automatic-mowing-input.txt";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getAutomaticMowingResult(args.length > 0 ? args[0] : DEFAULT_FILE_NAME));
+    }
+
+    public static List<String> getAutomaticMowingResult(String fileName) throws Exception{
+        IMowInputParser parser = new MowInputParser();
+        MowingAreaService mowingAreaService = new MowingAreaService();
+        var mowerTasks = parser.parseMowingTasks(fileName);
+        mowingAreaService.mowSequentially(mowerTasks);
+        return mowerTasks.stream().map(mowerTask -> mowerTask.mower().toString()).toList();
     }
 }
--- src/test/java/org/mowitnow/automaticmower/application/AutomaticMowerClientTest.java
+++ src/test/java/org/mowitnow/automaticmower/application/AutomaticMowerClientTest.java
@@ -1,18 +1,22 @@
 package org.mowitnow.automaticmower.application;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mowitnow.automaticmower.domain.*;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
 
 public class AutomaticMowerClientTest {
 
     @Test
-    public void should_mow_automatically_the_area_based_on_instructions_defined_in_file() {
+    public void should_mow_automatically_the_area_based_on_instructions_defined_in_file() throws Exception {
         // GIVEN
+        var fileName = "automatic-mowing-input.txt";
 
         // WHEN
+        var result = AutomaticMowerClient.getAutomaticMowingResult(fileName);
 
         // THEN
-
+        Assertions.assertEquals(List.of("1 3 N", "5 1 E"), result);
     }
 }
